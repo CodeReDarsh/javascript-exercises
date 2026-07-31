1fba35da4f55d35bd3ddc772963d246c52728d0d
cdr <adarshcp2077@gmail.com>
2026-07-30 20:28:05 -0400

    test: added a dummy test to practice making tests in a test suite.

--- foundations/01_helloWorld/helloWorld.spec.js
+++ foundations/01_helloWorld/helloWorld.spec.js
@@ -4,4 +4,8 @@ describe('Hello World', function() {
   test('says "Hello, World!"', function() {
     expect(helloWorld()).toEqual('Hello, World!');
   });
+
+  test('dummy', function () {
+    expect("test string").toEqual('test string');
+  });
 });
