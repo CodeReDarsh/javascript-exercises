4fc287304bb2800b404c880f203a273a15440a6d
cdr <adarshcp2077@gmail.com>
2026-08-04 23:42:24 -0400

    feat: implemented function to generate a strings of repeats given an input string

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
+    arrOfRepeats = [""]
+    
+    for (let i = 0; i < num; i++) {
+        arrOfRepeats.push(string)
+    }
+    
+    return arrOfRepeats.join("");
 };
 
 // Do not edit below this line
