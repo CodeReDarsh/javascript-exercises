a46c1d8ca492584d6fb75b9cbee7e10ca9478373
cdr <adarshcp2077@gmail.com>
2026-09-10 02:30:20 -0400

    feat: completed object_basics/01_calculator exercise

--- foundations/object_basics/01_calculator/calculator.js
+++ foundations/object_basics/01_calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((sum, currentVal) => sum + currentVal, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((prod, currentVal) => prod * currentVal, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(n, prod = 1) {
+  if (n === 0) {
+    return prod;
+  }
+  return factorial(n - 1, prod * n);
 };
 
 // Do not edit below this line
