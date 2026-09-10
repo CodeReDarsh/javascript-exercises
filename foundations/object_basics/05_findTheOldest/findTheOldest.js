8a5802e2e1b3bcc891c0eb2cdb698f91e79857a7
cdr <adarshcp2077@gmail.com>
2026-09-10 17:06:09 -0400

    feat: completed object_basics/05_findTheOldest. All object basics exercises completed.

--- foundations/object_basics/05_findTheOldest/findTheOldest.js
+++ foundations/object_basics/05_findTheOldest/findTheOldest.js
@@ -1,5 +1,27 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  return people.reduce(
+    (oldest, person) => {
+      let personDeathYear = new Date().getFullYear();
+      if ("yearOfDeath" in person) {
+        personDeathYear = person.yearOfDeath;
+      }
+      let oldestDeathYear = personDeathYear;
+      if ("yearOfDeath" in oldest) {
+        oldestDeathYear = oldest.yearOfDeath;
+      }
+      if (
+        personDeathYear - person.yearOfBirth >
+        oldestDeathYear - oldest.yearOfBirth
+      ) {
+        oldest = person;
+      }
+      return oldest;
+    },
+    {
+      yearOfBirth: new Date().getFullYear(),
+      yearOfDeath: new Date().getFullYear(),
+    },
+  );
 };
 
 // Do not edit below this line
