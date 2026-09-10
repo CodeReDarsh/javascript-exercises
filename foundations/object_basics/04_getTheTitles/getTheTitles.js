7ef81db0bc5941ff451a8fba604f859eafaed9c5
cdr <adarshcp2077@gmail.com>
2026-09-10 15:56:56 -0400

    feat: completed object_basics/04_getTheTitles

--- foundations/object_basics/04_getTheTitles/getTheTitles.js
+++ foundations/object_basics/04_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+  return array.reduce((titles, obj) => {
+    titles.push(obj.title);
+    return titles;
+  }, []);
 };
 
 // Do not edit below this line
