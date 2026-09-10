4a8a32a53b234ae158087bb02bae490562abe75a
cdr <adarshcp2077@gmail.com>
2026-09-10 05:34:04 -0400

    feat: completed exercise object_basics/03_fibonacci.

--- foundations/object_basics/03_fibonacci/fibonacci.js
+++ foundations/object_basics/03_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+  if (!/^[0-9]+$/.test(n)) {
+    return "OOPS";
+  }
+  n = Number(n);
+  if (n === 0) {
+    return 0;
+  } else if (n === 1 || n === 2) {
+    return 1;
+  }
+  let prevFib = 1;
+  let curFib = 1;
+  for (let m = 3; m <= n; m++) {
+    curFib += prevFib;
+    prevFib = curFib - prevFib;
+  }
+  return curFib;
 };
 
 // Do not edit below this line
