7fd519b5c089f42531ecfe5d823dcc114482c83c
cdr <adarshcp2077@gmail.com>
2026-09-10 04:19:38 -0400

    feat: completed object_basics/02_palindromes

--- foundations/object_basics/02_palindromes/palindromes.js
+++ foundations/object_basics/02_palindromes/palindromes.js
@@ -1,5 +1,30 @@
-const palindromes = function () {
+const palindromes = function (s) {
+  const string = [];
+  const isAlnum = (s) => /^[a-z0-9]+$/i.test(s);
+  /*
+    /^[a-z0-9]+$/i
+    ^         Start of string
+    [a-z0-9]  a or b or c or ... z or 0 or 1 or ... 9
+    +         one or more times (change to * to allow empty string)
+    $         end of string
+    /i        case-insensitive
+*/
 
+  for (let c of s) {
+    if (isAlnum((c = c.toLowerCase()))) {
+      string.push(c);
+    }
+  }
+  s = string.join("");
+  for (let i = 0; i <= Math.floor(s.length / 2); i++) {
+    if (
+      (isAlnum(s[i]) || isAlnum(s[s.length - i - 1])) &&
+      s[i] !== s[s.length - i - 1]
+    ) {
+      return false;
+    }
+  }
+  return true;
 };
 
 // Do not edit below this line
